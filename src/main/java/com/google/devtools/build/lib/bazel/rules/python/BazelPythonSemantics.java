f9fcc10fe1a90f577a573064eb4b778231416558
Fahrzin Hemmati <fahhem@gmail.com>
2018-11-07 19:39:17 -0800

    Enable custom py_binary stub_template attribute

--- src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java
@@ -65,8 +65,6 @@ public class BazelPythonSemantics implements PythonSemantics {
 
   public static final Runfiles.EmptyFilesSupplier GET_INIT_PY_FILES =
       new PythonUtils.GetInitPyFiles((Predicate<PathFragment> & Serializable) source -> false);
-  private static final Template STUB_TEMPLATE =
-      Template.forResource(BazelPythonSemantics.class, "python_stub_template.txt");
   public static final InstrumentationSpec PYTHON_COLLECTION_SPEC =
       new InstrumentationSpec(FileTypeSet.of(BazelPyRuleClasses.PYTHON_SOURCE))
           .withSourceAttributes("srcs")
@@ -164,6 +162,7 @@ public class BazelPythonSemantics implements PythonSemantics {
     // workspace-relative path. On Windows this is also passed to the launcher to use for the
     // first-stage.
     String pythonBinary = getPythonBinary(ruleContext, common, bazelConfig);
+    Artifact stubTemplateArtifact = ruleContext.getPrerequisiteArtifact("stub_template");
 
     // Version information for host config diagnostic warning.
     PythonVersion attrVersion = PyCommon.readPythonVersionFromAttribute(ruleContext.attributes());
@@ -176,8 +175,8 @@ public class BazelPythonSemantics implements PythonSemantics {
     ruleContext.registerAction(
         new TemplateExpansionAction(
             ruleContext.getActionOwner(),
+            stubTemplateArtifact,
             stubOutput,
-            STUB_TEMPLATE,
             ImmutableList.of(
                 Substitution.of(
                     "%main%", common.determineMainExecutableSource(/*withWorkspaceName=*/ true)),
